e5b1efa6baf602b11e8074ce06050188296520dc
Ivan Alejandro <ivanalejandro0@gmail.com>
2015-10-05 17:14:32 -0300

    Update script to comply own template

--- apps/sqlitebrowser/app.sh
+++ apps/sqlitebrowser/app.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# NOTE: to get X11 socket forwarding to work we need this
-xhost local:root
+source $BASE_APP
+DATA_DIR="$DATA_ROOT/sqlitebrowser/"
 
 docker run --rm -it \
-    --net host \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    -v `pwd`:/host/ \
+    -v $DATA_DIR:/host/ \
     test/sqlitebrowser
